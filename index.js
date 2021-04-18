0a638da17aea2306a61360270f035e0673f862a7
ntlarry <82742478+ntlarry@users.noreply.github.com>
2021-04-17 21:38:28 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function generate_iframe() {             
+                var div = document.createElement('div');
+                div.innerHTML="<iframe width='420' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ?t=42&autoplay=1'> </iframe>"
+                document.getElementById("div1").appendChild(div);
+                var p = document.createElement('p');
+                p.innerHTML="test";
+            }
+            var videoPlayButtons = document.querySelectorAll("button")
+
+            for (var i=0; i<videoPlayButtons.length; i++){
+                videoPlayButtons[i].addEventListener('click', generate_iframe);
+            }
