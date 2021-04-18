c43f553d8ff837778e25b006be6b5c5742be056d
ntlarry <82742478+ntlarry@users.noreply.github.com>
2021-04-18 15:15:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function generate_iframe() {             
-                var div = document.createElement('div');
-                div.innerHTML="<iframe width='420' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ?t=42&autoplay=1'> </iframe>"
-                document.getElementById("div1").appendChild(div);
-                var p = document.createElement('p');
-                p.innerHTML="test";
-            }
-            var videoPlayButtons = document.querySelectorAll("button")
+    var div = document.createElement('div');
+    div.innerHTML="<iframe width='420' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ?t=42&autoplay=1'> </iframe>"
+    document.getElementById("div1").appendChild(div);
+    var p = document.createElement('p');
+    p.innerHTML="test";
+}
 
-            for (var i=0; i<videoPlayButtons.length; i++){
-                videoPlayButtons[i].addEventListener('click', generate_iframe);
-            }
+window.addEventListener('load', (event) => {
+    var videoPlayButtons = document.querySelectorAll("button")
+    for (var i=0; i<videoPlayButtons.length; i++){
+        videoPlayButtons[i].addEventListener('click', generate_iframe);
+    }
+});
+
