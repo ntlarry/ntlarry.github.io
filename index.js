f132eeca93e306522c37024aee7070978fd150d4
ntlarry <82742478+ntlarry@users.noreply.github.com>
2021-04-18 20:06:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function generate_iframe() {
     div.innerHTML="<iframe width='420' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ?t=42&autoplay=1'> </iframe>"
     document.getElementById("div1").appendChild(div);
     document.getElementById("text").innerHTML = "This section will continue to go into detail about the psychological basis of Enneagram tests specifically in relation to Enneagram Type 6 individuals. In Enneagram theory, which is considered by many to have <span class='b'>never</span> been a wholly scientifically-based theory, Type 6 individuals are those who are <span class='b'>gonna give</span> their all no matter what <span class='b'>you</span> or anyone say. In corporate situations, they climb <span class='b'>up</span> the ladder by being loyal to their friends and their colleagues. Those people are characterized by how they <span class='b'>never gonna let</span> everyone, including <span class='b'>you</span>, be attacked or put <span class='b'>down</span>. However, they can be very obstinate, <span class='b'>never</span> changing their mind once they have decided on something. They are <span class='b'>gonna</span> keep going even if everyone else decides to <span class='b'>turn around</span>, which means that they may not always be logical <span class='b'>and</span> calm. However, they will never <span class='b'>desert you</span>. Please scroll down for an example of the characterization of Enneagram Type 6."
+    window.scrollBy(0, 100);
 }
 
 // make sure that everything is loaded (only when everything is loaded, run the code)
